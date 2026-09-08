080739e79c2622c85dc1518d3a9b421d55627d39
daxifraud <weixy1040593615@gmail.com>
2026-09-08 22:09:07 +0800

    Delete armored-forces-fixed directory

--- armored-forces-fixed/js/debug-flag.js
+++
@@ -1,5 +0,0 @@
-
-/* 调试/自动基线引导:?debug=1 或 ?autotest=midgame → __TANK_DEBUG
-   (core.js 的 DBG_ON 在加载期快照,必须在其加载前置位) */
-if (/[?&](debug=1|autotest=midgame)/.test(location.search)) window.__TANK_DEBUG = true;
-
